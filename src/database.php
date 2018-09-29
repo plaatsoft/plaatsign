5ad1e3aa65d167bb462d32d2ffd1ca98dbbf0840
Willem van der Plaat <wplaat@gmail.com>
2018-09-29 15:18:39 +0200

    Add missing part

--- src/database.php
+++ src/database.php
@@ -240,6 +240,12 @@ function plaatsign_db_check_version() {
 		$version="0.5";
       plaatsign_db_execute_sql_file($version);
    }
+   
+   // Path database if needed	
+   if ($version=="0.5") {
+		$version="1.0";
+      plaatsign_db_execute_sql_file($version);
+   }
 }
 
 /*
